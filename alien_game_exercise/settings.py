9835386d5bc457f3574136aa98ff8460927651b3
Philgang <845223897@qq.com>
2019-09-29 10:31:01 +0800

    add alien

---
+++ alien_game_exercise/settings.py
@@ -0,0 +1,13 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1500
+        self.screen_height = 1000
+        self.bg_color = (255, 255, 250)
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.bullet_width = 6
+        self.bullet_height = 20
+        self.bullet_color = 10, 10, 10,
+        self.alien_speed_factor = 3
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
